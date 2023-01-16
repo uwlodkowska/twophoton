ff8b1285a85fd0a54c38af07b77920cb02ac6ff3
uwlodkowska <u.wlodkowska@nencki.edu.pl>
2023-01-16 17:00:07 +0100

    sketch of overlap definition comparison

--- alignment/cell_preprocessing.py
+++ alignment/cell_preprocessing.py
@@ -6,6 +6,10 @@ This is a temporary script file.
 """
 from skimage.draw import disk
 import numpy as np
+import pandas as pd
+from skimage import io
+
+dir_path = "/media/ula/DATADRIVE1/fos_gfp_tmaze/ctx_landmark/despeckle/alignment_result/aligned_despeckle/"
 
 
 ROI_DIAMETER = [8,7,4]
@@ -17,6 +21,8 @@ XY_SCALE = 1.19;
 Z_SCALE = 2;
 
 single_session_cell_data_fn = "m{}r{}_{}_output.txt"#output from icy
+cell_data_fn_template = "m{}r{}_{}_output.txt"
+img_fn_template = "m{}r{}_{}.tif"
 
 
 def calculate_intensity(center_coords, img):
@@ -33,11 +39,10 @@ def calculate_intensity(center_coords, img):
     return sum_int/area_int
 
 def filter_unstable_intensity(df, img):
-    df["intensity_standarized"] = df.apply(lambda row : 
-                                           calculate_intensity([row[ICY_COLNAMES['xcol']],
-                                                               row[ICY_COLNAMES['ycol']],
-                                                               row[ICY_COLNAMES['zcol']]],
-                                                               img), axis = 1)
+    df["intensity_standarized"] = calculate_intensity([df[ICY_COLNAMES['xcol']],
+                                                               df[ICY_COLNAMES['ycol']],
+                                                               df[ICY_COLNAMES['zcol']]],
+                                                               img)
     return df[df['Mean Intensity (ch 0)']/df['intensity_standarized']<=1.5]
 
 
@@ -47,5 +52,29 @@ def pixels_to_um(df):
     df[ICY_COLNAMES['zcol']] = df[ICY_COLNAMES['zcol']]*Z_SCALE
     return df
 
-def find_session_ovlap(mouse, region, s1_idx, s2_idx):
+def find_overlap(mouse, region, s_idxses, session_order):
+    dfs = dict.fromkeys(s_idxses)
+    imgs = dict.fromkeys(s_idxses)
+    for i in s_idxses:
+        dfs[i] = pd.read_csv(dir_path + cell_data_fn_template
+                             .format(mouse, region, session_order[i]), "\t", header=1)
+        imgs[i] = io.imread(dir_path + img_fn_template
+                             .format(mouse, region, session_order[i])).astype("uint8")
+    #dokonczyc przepisywanie oryginalnego porownania z nb
+    #wektoryzacja
+    
+def test_fun(mouse, region, s_idxses, session_order):
+    pd.read_csv(dir_path + cell_data_fn_template
+                .format(mouse, region, session_order[s_idxses[0]]+"_"+session_order[s_idxses[1]]))
+    df = pd.read_csv(dir_path + cell_data_fn_template
+                     .format(mouse, region, session_order[s_idxses[0]]), "\t", header=1)
+    img = io.imread(dir_path + img_fn_template
+                    .format(mouse, region, session_order[s_idxses[1]])).astype("uint8")
+    calculate_intensity([df[ICY_COLNAMES['xcol']],
+                                                               df[ICY_COLNAMES['ycol']],
+                                                               df[ICY_COLNAMES['zcol']]],
+                                                               img)
+    
+
+    
     
