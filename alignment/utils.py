2c1a8f84244957f9fee73e1d3210f6b1b594befc
uwlodkowska <u.wlodkowska@nencki.edu.pl>
2023-02-22 17:57:43 +0100

    adding utils file

---
+++ alignment/utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 17:19:59 2023
+
+@author: ula
+"""
+import pandas as pd
+from constants import dir_path, FILENAMES
+from skimage import io
+
+def read_single_session_cell_data(mouse, region, sessions):
+    ret = dict.fromkeys(sessions)
+    for s in sessions:
+        df = pd.read_csv(dir_path + FILENAMES['cell_data_fn_template']
+                             .format(mouse, region, s), "\t", header=1)
+        if len(sessions) == 1:
+            return df
+        ret[s] = df
+    return ret
+    
+        
+def read_image(mouse, region, session):
+    return io.imread(dir_path + FILENAMES['img_fn_template']
+                     .format(mouse, region, session))
