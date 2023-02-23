6aa8e0ed6a0cda6f96a43737c0e58baaafcbe4eb
uwlodkowska <u.wlodkowska@nencki.edu.pl>
2023-02-23 16:56:15 +0100

    finish merge based distance calculation

--- alignment/intersession.py
+++ alignment/intersession.py
@@ -7,27 +7,19 @@ Created on Fri Feb 17 20:27:56 2023
 """
 import numpy as np
 import pandas as pd
-from constants import ICY_COLNAMES
+import utils
+import constants
 
 def identify_persistent_cells(mouse, region, sessions):
-    res_df = pd.DataFrame(columns=["idx1", "idx2", "intensity1", "intensity2"])
-    dfs = 
-    '''
-    multiple_algs = 0
-    for idx1, row1 in df1.iterrows():
-        rep_counter = 0
-        for idx2, row2 in df2.iterrows():
-            dist = np.linalg.norm(row1[xcolname:zcolname]-row2[xcolname:zcolname])
-            if dist < tolerance:
-                rep_counter += 1
-                if rep_counter == 2:
-                    multiple_algs+=1
-                elif rep_counter == 1:
-                    res_df = res_df.append({"idx1":idx1, "idx2" :idx2, 
-                                            "intensity1":row1['intensity_standarized'], 
-                                            "intensity2":row2['intensity_standarized']},
-                                           ignore_index=True)
-                    '''
-    return res_df
+    #tu dodac standaryzacje intensity
+    for i, s in enumerate(sessions):
+        sessions[i] = s[constants.COORDS_3D].reset_index()
+    cross_prod = sessions[0].merge(sessions[1], how = "cross", suffixes=("_s1", "_s2"))
+    cross_prod['distance'] = np.linalg.norm(cross_prod[[n+"_s1" for n in constants.COORDS_3D]].values
+                                            -cross_prod[[n+"_s2" for n in constants.COORDS_3D]].values, axis=1)
+    cross_prod = cross_prod[cross_prod.distance < constants.TOLERANCE]
 
-identify_persistent_cells(10,1,[])
+    return cross_prod
+
+identify_persistent_cells(10,1, utils.read_single_session_cell_data(
+                              10, 1, constants.CTX_FIRST_SESSIONS[:2]))
