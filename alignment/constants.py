2dabc54719130db33e1a558b657e64c4a1fd3475
uwlodkowska <u.wlodkowska@nencki.edu.pl>
2023-02-23 16:55:49 +0100

    constants update

--- alignment/constants.py
+++ alignment/constants.py
@@ -17,12 +17,14 @@ ICY_COLNAMES = {'mean_intensity' : 'Mean Intensity (ch 0)',
 XY_SCALE = 1.19
 Z_SCALE = 2
 
+COORDS_3D = [ICY_COLNAMES['xcol'], ICY_COLNAMES['ycol'], ICY_COLNAMES['zcol']]
+
 SCALE = [Z_SCALE, XY_SCALE, XY_SCALE]
 
 FILENAMES = {'single_session_cell_data_fn' : "m{}r{}_{}_output.txt",#output from icy,
              'cell_data_fn_template' : "m{}r{}_{}_output.txt",
              'img_fn_template' : "m{}r{}_{}.tif"}
 
-TOLERANCE = 5
+TOLERANCE = 4
 
 CTX_FIRST_SESSIONS = ['ctx', 'landmark1', 'landmark2']
